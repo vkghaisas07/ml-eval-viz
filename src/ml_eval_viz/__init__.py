756d52250c58a60f7afde84fc889104764a66d0f
vkghaisas07 <Vaibhav.K.Ghaisas@student.uts.edu.au>
2025-09-30 14:51:33 +1000

    fixed_imports

--- src/ml_eval_viz/__init__.py
+++ src/ml_eval_viz/__init__.py
@@ -1,6 +1,6 @@
 from .metrics import classification_metrics, regression_metrics
 from .plots import plot_confusion_matrix, plot_roc, plot_pr, plot_residuals, plot_feature_importance
-from .sklearn_helpers import evaluate_classifier
+from .sklearn_helpers import evaluate_classifier, evaluate_regressor
 
 __all__ = [
     "classification_metrics",
